e840cbe5f04bdc97ed9e33a937ff56a2df28ae50
shio-yaamaa <shio.yama520@gmail.com>
2021-04-11 22:23:19 +0900

    Output directory tree

--- packages/analyze/src/gitCommands.ts
+++ packages/analyze/src/gitCommands.ts
@@ -1,8 +1,8 @@
 import { exec } from 'child_process';
 
-export const listFiles = async (): Promise<String> => {
+export const listFiles = async (workingDirectory: string): Promise<String> => {
   return new Promise((resolve, reject) => {
-    exec('git ls-files', (error, stdout, stderr) => {
+    exec('git ls-files', { cwd: workingDirectory }, (error, stdout, stderr) => {
       if (error) {
         reject(error);
       }
--- packages/analyze/src/main.ts
+++ packages/analyze/src/main.ts
@@ -1,11 +1,19 @@
 import * as path from 'path';
+import * as minimist from 'minimist';
+import * as fsextra from 'fs-extra';
 
 import { FileItem } from './types';
 import { listFiles } from './gitCommands';
 import { buildDirectoryTree, directoryTreeToJSON } from './directoryTree';
 
 const main = async (): Promise<void> => {
-  let listFilesOutput = await listFiles();
+  const args = minimist(process.argv.slice(2));
+  const targetDirectory = args._[0];
+  if (!targetDirectory || targetDirectory.length === 0) {
+    console.error('Target directory is required');
+    return;
+  }
+  let listFilesOutput = await listFiles(targetDirectory);
   let files: FileItem[] = listFilesOutput
     .split('\n')
     .filter((filePath) => filePath.length > 0)
@@ -15,7 +23,10 @@ const main = async (): Promise<void> => {
       path: filePath,
     }));
   const directoryTree = buildDirectoryTree(files);
-  console.log(JSON.stringify(directoryTreeToJSON(directoryTree)));
+  fsextra.outputJSONSync(
+    path.join(__dirname, '../output/directoryTree.json'),
+    directoryTreeToJSON(directoryTree)
+  );
 };
 
 main();
