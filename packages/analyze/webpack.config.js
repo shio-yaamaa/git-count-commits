5b089d59cebd2af22ba364879cd27d9f5e9387b0
shio-yaamaa <shio.yama520@gmail.com>
2021-04-11 17:17:55 +0900

    Initialize TypeScript project

---
+++ packages/analyze/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "development",
+  target: "node",
+  entry: "./src/main.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
